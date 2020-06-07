c6a23d968679f4bf2cabfad0ca83b91c6307fe7a
Jay Nankani <39469570+jaysn1@users.noreply.github.com>
2020-06-06 21:32:09 -0700

    Color update

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -21,6 +21,22 @@ class ItemList extends Component{
     render(){
         
         const { items } = this.props.item;
+        let d = new Date();
+        let day = String(d.getDate());
+        let month = String(d.getMonth() + 1);
+        let year = String(d.getFullYear());
+        console.log(month);
+        if(day.length === 1){
+            day = "0" + day;
+        }
+        
+        if(month.length === 1){
+            month = "0" + month;
+        }
+        let current_date = Number(year+month+day);
+        console.log("curr",current_date);
+        //console.log(current_date > 20200614);
+
 
         return(
             <Container>
@@ -38,9 +54,11 @@ class ItemList extends Component{
                                                 &times;
                                         </Button>
                                         {name}
+                                        
+                                        
                                         <Button 
                                             className="date-btn"
-                                            // color={console.log(expiry_date) ? "danger":"info"}
+                                            color={current_date < Number(expiry_date.split("T")[0].split("-").join('')) ?"info":"danger"}
                                             >
                                                 <Moment>{expiry_date}</Moment>
                                         </Button>
